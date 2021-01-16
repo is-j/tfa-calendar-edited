0ed8121f6d57edb6560cad2e84fc5f309ae4d26e
Dennis Eum <dseum22@gmail.com>
2021-01-16 12:10:30 -0600

    nginx change

--- docker/webstart.sh
+++ docker/webstart.sh
@@ -6,13 +6,13 @@ fi
 
 while ! pgrep -x "php-fpm" > /dev/null
 do
-    if pgrep -x "php-fpm" > /dev/null && ! pgrep -x "nginx" > /dev/null
+    if pgrep -x "php-fpm" > /dev/null
     then
         supervisorctl start nginx
     fi
 done
 
-if pgrep -x "php-fpm" > /dev/null && ! pgrep -x "nginx" > /dev/null
+if pgrep -x "php-fpm" > /dev/null
 then
     supervisorctl start nginx
 fi
