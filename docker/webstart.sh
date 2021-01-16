a96610a2493f37cd979f0879c7a2404fc3980997
Dennis Eum <dseum22@gmail.com>
2021-01-16 14:21:45 -0600

    debugging

--- docker/webstart.sh
+++ docker/webstart.sh
@@ -3,14 +3,16 @@ if ! pgrep -x "php-fpm" > /dev/null
 then
     supervisorctl start php-fpm
 fi
-
+echo "hello1"
 while ! pgrep -x "php-fpm" > /dev/null
 do
     sleep .01
 done
 
+echo "hello2"
 if ! pgrep -x "nginx" > /dev/null
 then
     supervisorctl start nginx
 fi
 
+echo "hello3"
