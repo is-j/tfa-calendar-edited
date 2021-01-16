d5cbd83dd8f20e6754728afdc97047a7a1bf3c24
Dennis Eum <dseum22@gmail.com>
2021-01-16 14:48:10 -0600

    simpler script

--- docker/webstart.sh
+++ docker/webstart.sh
@@ -3,16 +3,12 @@ if ! pgrep -x "php-fpm" > /dev/null
 then
     supervisorctl start php-fpm
 fi
-echo "hello1"
-while ! pgrep -x "php-fpm" > /dev/null
+
+while :
 do
-    sleep .01
+    if pgrep -x "php-fpm" > /dev/null
+    then
+        supervisorctl start nginx
+        exit
+    fi
 done
-
-echo "hello2"
-if ! pgrep -x "nginx" > /dev/null
-then
-    supervisorctl start nginx
-fi
-
-echo "hello3"
