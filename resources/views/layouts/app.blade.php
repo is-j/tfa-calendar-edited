aa09479f95eebf0b573b18c5c77f1afb06c0c6e7
Dennis Eum <dseum22@gmail.com>
2021-01-16 21:41:54 -0600

    favicon fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,8 +16,8 @@ $name = Route::currentRouteName();
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- favicon -->
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon-16x16.png') }}">
 
     <title>{{ config('app.name', 'tfa-calendar') }}</title>
 
