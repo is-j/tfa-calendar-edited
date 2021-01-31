b757d4f51d92f9da0f93056ffd92a0512b1a9f63
dseum22 <dseum22@gmail.com>
2021-01-30 19:42:46 -0600

    Cleaned up unnecessary https forcescheme

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -17,7 +17,7 @@ class HttpsProtocol
      */
     public function handle(Request $request, Closure $next)
     {
-        if (!$request->secure() && App::environment() === 'production') {
+        if (!$request->secure() && !App::isLocal()) {
             return redirect()->secure($request->getRequestUri());
         }
         return $next($request);
